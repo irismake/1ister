efb5dcc758e6eb2a62bd6318352becbfa33b25fb
irismake <iris3455@gmail.com>
2024-01-07 18:10:20 +0900

    Refactor : email login page 버튼

--- lib/login/email_login_page.dart
+++ lib/login/email_login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../model/custom_text_form_field.dart';
+
 class EmailLoginPage extends StatefulWidget {
   const EmailLoginPage({Key? key}) : super(key: key);
 
@@ -13,7 +15,6 @@ class _EmailLoginScreenState extends State<EmailLoginPage> {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       builder: (BuildContext context, child) => MaterialApp(
-        title: 'Flutter Demo',
         home: _EmailLoginWidget(),
         debugShowCheckedModeBanner: false,
       ),
@@ -37,11 +38,8 @@ class EmailLoginWidget extends State<_EmailLoginWidget> {
   final noFocusTextColor = Color(0xff868E96);
   final brandPointColor = Color(0xff5BFF7F);
 
-  Color _emailTextColor = Colors.black;
-  Color _passwordTextColor = Colors.black;
 
-  String inputEmail = '';
-  String inputPassword = '';
+
   String finalEmail = '';
   String finalPassword = '';
 
@@ -52,54 +50,11 @@ class EmailLoginWidget extends State<_EmailLoginWidget> {
   FocusNode _emailFocus = FocusNode();
   FocusNode _passwordFocus = FocusNode();
 
-  TextEditingController _inputEmailController = TextEditingController();
-  TextEditingController _inputPasswordController = TextEditingController();
-
   @override
   void initState() {
     super.initState();
 
-    _inputEmailController.addListener(() {
-      setState(() {
-        if (_inputEmailController.text.isNotEmpty) {
-          _emailState = true;
-          if (_passwordState == true) {
-            _loginButtonState = true;
-          }
-        } else {
-          _emailState = false;
-          _loginButtonState = false;
-        }
-      });
-    });
 
-    _inputPasswordController.addListener(() {
-      setState(() {
-        if (_inputPasswordController.text.isNotEmpty) {
-          _passwordState = true;
-          if (_emailState == true) {
-            _loginButtonState = true;
-          }
-        } else {
-          _passwordState = false;
-          _loginButtonState = false;
-        }
-      });
-    });
-
-    _emailFocus.addListener(() {
-      setState(() {
-        _emailTextColor =
-            _emailFocus.hasFocus ? Colors.black : noFocusTextColor;
-      });
-    });
-
-    _passwordFocus.addListener(() {
-      setState(() {
-        _passwordTextColor =
-            _passwordFocus.hasFocus ? Colors.black : noFocusTextColor;
-      });
-    });
   }
 
   @override
@@ -141,84 +96,47 @@ class EmailLoginWidget extends State<_EmailLoginWidget> {
                     Column(
                       // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        TextField(
-                          style: TextStyle(
-                            fontFamily: 'PretendardRegular',
-                            decorationThickness: 0,
-                            fontSize: 16.sp,
-                            color: _emailTextColor,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          showCursor: false,
-                          controller: _inputEmailController,
+                        CustomTextFormField(
+                          hintText: '이메일',
                           focusNode: _emailFocus,
-                          decoration: InputDecoration(
-                            isDense: true,
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 20.0.h, horizontal: 16.w),
-                            hintText: '이메일',
-                            hintStyle: TextStyle(
-                              fontFamily: 'PretendardRegular',
-                              color: noFocusColor,
-                              fontSize: 16.sp,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4)),
-                              borderSide: BorderSide(
-                                  width: 1.w, color: brandPointColor),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4)),
-                              borderSide:
-                                  BorderSide(width: 1.w, color: noFocusColor),
-                            ),
-                          ),
-                          keyboardType: TextInputType.emailAddress,
+                          onChanged: (value) {
+                            setState(() {
+                              finalEmail = value!;
+                              if (value!.isNotEmpty) {
+                                _emailState = true;
+                                if (_emailState) {
+                                  _loginButtonState = true;
+                                }
+                              } else {
+                                _emailState = false;
+                                _loginButtonState = false;
+                              }
+                            });
+                          },
+                          keyboardType: TextInputType.text,
                         ),
                         SizedBox(
                           height: 12.h,
                         ),
-                        TextField(
-                          style: TextStyle(
-                            fontFamily: 'PretendardRegular',
-                            decorationThickness: 0,
-                            fontSize: 16.sp,
-                            color: _passwordTextColor,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          textAlign: TextAlign.start,
-                          controller: _inputPasswordController,
+                        CustomTextFormField(
+                          hintText: '비밀번호',
                           focusNode: _passwordFocus,
-                          showCursor: false,
-                          decoration: InputDecoration(
-                            isDense: true,
-                            contentPadding: EdgeInsets.symmetric(
-                                horizontal: 16.0.w, vertical: 20.h),
-                            hintText: '비밀번호',
-                            hintStyle: TextStyle(
-                              decorationThickness: 0,
-                              color: noFocusColor,
-                              fontSize: 16.sp,
-                              fontWeight: FontWeight.w600,
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4)),
-                              borderSide: BorderSide(
-                                  width: 1.w, color: brandPointColor),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4)),
-                              borderSide:
-                                  BorderSide(width: 1.w, color: noFocusColor),
-                            ),
-                          ),
+                          isObscureText:true,
+                          onChanged: (value) {
+                            setState(() {
+                              finalPassword = value!;
+                              if (value!.isNotEmpty) {
+                                _passwordState = true;
+                                if (_passwordState) {
+                                  _loginButtonState = true;
+                                }
+                              } else {
+                                _passwordState = false;
+                                _loginButtonState = false;
+                              }
+                            });
+                          },
                           keyboardType: TextInputType.text,
-                          obscureText: true,
                         ),
                       ],
                     ),
@@ -231,8 +149,6 @@ class EmailLoginWidget extends State<_EmailLoginWidget> {
                   child: TextButton(
                     onPressed: () {
                       setState(() {
-                        finalEmail = _inputEmailController.text;
-                        finalPassword = _inputPasswordController.text;
                         print("[final email] : $finalEmail");
                         print("[final password] : $finalPassword");
                       });
