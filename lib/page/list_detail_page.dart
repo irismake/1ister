452131c79fad7720eb31aab211505cc77709c0d8
irismake <iris3455@gmail.com>
2024-02-20 21:36:27 +0900

    feat : Implement main list detail

--- lib/page/list_detail_page.dart
+++ lib/page/list_detail_page.dart
@@ -5,23 +5,21 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../widget/custom/custom_keyword.dart';
 import '../widget/custom_app_bar.dart';
 
-class ListDetailPage extends StatefulWidget {
-  ListDetailPage({Key? key}) : super(key: key);
+class ListDetailPage extends StatelessWidget {
+  final title;
+  final description;
+  final userName;
+  final updateDate;
+  final List<String> keywords;
 
-  @override
-  State<ListDetailPage> createState() => _ListDetailPage();
-}
-
-class _ListDetailPage extends State<ListDetailPage> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
+  ListDetailPage(
+      {Key? key,
+      required this.title,
+      required this.description,
+      required this.userName,
+      required this.updateDate,
+      required this.keywords})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -47,20 +45,17 @@ class _ListDetailPage extends State<ListDetailPage> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      SizedBox(
-                        height: 34.0.h,
-                        child: const Center(
-                          child: Text(
-                            '후쿠오카 백화점 쇼핑 리스트',
-                            style: TextStyle(
-                              color: Color(0xFF343A40),
-                              fontSize: 24,
-                              fontFamily: 'Pretendard',
-                              fontWeight: FontWeight.w700,
-                              height: 0.06,
-                            ),
-                          ),
+                      Text(
+                        title,
+                        style: TextStyle(
+                          color: Color(0xFF343A40),
+                          fontSize: 24.sp,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w700,
+                          height: 1.4.h,
                         ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 10,
                       ),
                       SizedBox(
                         height: 32.0.h,
@@ -80,27 +75,21 @@ class _ListDetailPage extends State<ListDetailPage> {
                   SizedBox(
                     width: double.infinity,
                     child: Wrap(
-                      direction: Axis.horizontal, // 나열 방향
-                      alignment: WrapAlignment.start, // 정렬 방식
-                      spacing: 5, // 좌우 간격
-                      runSpacing: 5, // 상하 간격
-                      children: [
-                        KeyWord(keyWordName: '후쿠오카'),
-                        KeyWord(keyWordName: '쇼핑'),
-                        KeyWord(keyWordName: '백화점'),
-                        KeyWord(keyWordName: '택스프리'),
-                        KeyWord(keyWordName: '일본'),
-                        KeyWord(keyWordName: '후쿠오카'),
-                      ],
+                      direction: Axis.horizontal,
+                      alignment: WrapAlignment.start,
+                      spacing: 5,
+                      runSpacing: 5,
+                      children: keywords.map((keyword) {
+                        return KeyWord(keyWordName: keyword);
+                      }).toList(),
                     ),
                   ),
                   SizedBox(
                     height: 32.0.h,
                   ),
                   Container(
-                    //color: Colors.green,
                     child: Text(
-                      '후쿠오카 여행 갔을 때 가장 많이 사는 쇼핑리스트 백화점편입니다. 백화점에서는 택스프리도 받을 수 있어 할인율이 더 높아집니다.',
+                      description,
                       style: TextStyle(
                         color: Color(0xFF343A40),
                         fontSize: 15.sp,
@@ -119,7 +108,7 @@ class _ListDetailPage extends State<ListDetailPage> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Text(
-                        '부지런한 아보카도 ',
+                        userName,
                         style: TextStyle(
                           color: Color(0xFF868E96),
                           fontSize: 14.sp,
@@ -132,7 +121,7 @@ class _ListDetailPage extends State<ListDetailPage> {
                         width: 8.0.h,
                       ),
                       Text(
-                        '23.09.13',
+                        updateDate,
                         style: TextStyle(
                           color: Color(0xFF868E96),
                           fontSize: 14.sp,
--- lib/widget/list/home_list_view.dart
+++ lib/widget/list/home_list_view.dart
@@ -6,69 +6,100 @@ import 'package:intl/intl.dart';
 
 import '../../model/lists.dart';
 import '../../model/provider/main_lists_provider.dart';
+import '../../page/list_detail_page.dart';
+import '../../services/api_service.dart';
 import '../custom/custom_book_mark_button.dart';
 
 class HomeListView extends StatelessWidget {
   HomeListView({Key? key});
-
   @override
   Widget build(BuildContext context) {
     return Consumer<MainListsProvider>(builder: (context, provider, child) {
       final List<MainListData> mainLists = provider.mainLists();
-      print(mainLists);
       return ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: mainLists.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.only(right: 12.0.w),
-            child: SizedBox(
-              width: 160.0,
-              child: Stack(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 200.0,
-                        width: double.infinity,
-                        decoration: ShapeDecoration(
-                          color: Color(0xFFF1F3F5),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
+          return GestureDetector(
+            onTap: () async {
+              Map<String, dynamic> listData =
+                  await ApiService.getListDetail(mainLists[index].id);
+              final title = utf8.decode(listData['title'].toString().codeUnits);
+              final description =
+                  utf8.decode(listData['description'].toString().codeUnits);
+              final userName =
+                  utf8.decode(listData['user_name'].toString().codeUnits);
+              final updateDate = DateFormat('yy.MM.dd')
+                  .format(DateTime.parse(listData['updated_at']));
+              List<String> keywords = [];
+              keywords
+                  .add(utf8.decode(listData['keyword_1'].toString().codeUnits));
+              keywords
+                  .add(utf8.decode(listData['keyword_2'].toString().codeUnits));
+              print(keywords);
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ListDetailPage(
+                          title: title,
+                          description: description,
+                          userName: userName,
+                          updateDate: updateDate,
+                          keywords: keywords,
+                        )),
+              );
+            },
+            child: Padding(
+              padding: EdgeInsets.only(right: 12.0.w),
+              child: SizedBox(
+                width: 160.0,
+                child: Stack(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 200.0,
+                          width: double.infinity,
+                          decoration: ShapeDecoration(
+                            color: Color(0xFFF1F3F5),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8)),
+                          ),
                         ),
-                      ),
-                      Text(
-                        '${utf8.decode(mainLists[index].title.runes.toList())}',
-                        style: TextStyle(
-                          color: Color(0xFF343A40),
-                          fontSize: 16.sp,
-                          fontFamily: 'Pretendard',
-                          fontWeight: FontWeight.w600,
-                          height: 1.5.h,
+                        Text(
+                          '${utf8.decode(mainLists[index].title.runes.toList())}',
+                          style: TextStyle(
+                            color: Color(0xFF343A40),
+                            fontSize: 16.sp,
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w600,
+                            height: 1.5.h,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
                         ),
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 2,
-                      ),
-                      Text(
-                        '${DateFormat('yy.MM.dd').format(DateTime.parse(mainLists[index].updatedAt))}',
-                        style: TextStyle(
-                          color: Color(0xFF868E96),
-                          fontSize: 12.sp,
-                          fontFamily: 'Pretendard',
-                          fontWeight: FontWeight.w500,
-                          height: 1.5.h,
-                        ),
-                      )
-                    ],
-                  ),
-                  CustomBookMarkButton(
-                    index: index,
-                    listId: mainLists[index].id,
-                    isBookMarked: mainLists[index].isBookmarked,
-                  ),
-                ],
+                        Text(
+                          '${DateFormat('yy.MM.dd').format(DateTime.parse(mainLists[index].updatedAt))}',
+                          style: TextStyle(
+                            color: Color(0xFF868E96),
+                            fontSize: 12.sp,
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w500,
+                            height: 1.5.h,
+                          ),
+                        )
+                      ],
+                    ),
+                    CustomBookMarkButton(
+                      index: index,
+                      listId: mainLists[index].id,
+                      isBookMarked: mainLists[index].isBookmarked,
+                    ),
+                  ],
+                ),
               ),
             ),
           );
