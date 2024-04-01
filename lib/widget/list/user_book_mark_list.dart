7f2d30c717aa7014b04e44a9ab41b04a81d0fdbe
irismake <iris3455@gmail.com>
2024-04-01 19:12:24 +0900

    style : UserBookMarkList page UI/UX

--- lib/widget/list/user_book_mark_list.dart
+++ lib/widget/list/user_book_mark_list.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class UserBookMarkList extends StatelessWidget {
   const UserBookMarkList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(height: 60, width: 80, color: Colors.red);
+    return Container(
+      color: Color(0xffF8F9FA),
+      child: Center(
+        child: Text(
+          "아직 북마크 리스트가 비어있어요.",
+          style: TextStyle(
+            color: Color(0xff495057),
+            fontSize: 16.sp,
+            fontFamily: 'Pretendard',
+            fontWeight: FontWeight.w600,
+            height: 1.5.h,
+          ),
+        ),
+      ),
+    );
   }
 }
