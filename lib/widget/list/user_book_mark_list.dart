4012931a0fff3832ea6ef1d0a293f6b1943045c9
irismake <iris3455@gmail.com>
2024-04-05 23:05:04 +0900

    fix : Fix usersList builder overflow

--- lib/widget/list/user_book_mark_list.dart
+++ lib/widget/list/user_book_mark_list.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
@@ -35,105 +36,106 @@ class UserBookMarkList extends StatelessWidget {
                   ),
                 ),
               )
-            : GridView.builder(
+            : Padding(
                 padding: EdgeInsets.symmetric(
                   horizontal: 16.0.w,
                   vertical: 20.0.h,
                 ),
-                itemCount: usersBookmarkLists.length, //item 개수
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    childAspectRatio: 0.643,
-                    crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-                    crossAxisSpacing: 8.0.w,
-                    mainAxisSpacing: 16.0.h),
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () async {
-                      Map<String, dynamic> listData =
-                          await ApiService.getListDetail(
-                              usersBookmarkLists[index].id);
-                      final title =
-                          utf8.decode(listData['title'].toString().codeUnits);
-                      final description = utf8
-                          .decode(listData['description'].toString().codeUnits);
-                      final userName = utf8
-                          .decode(listData['user_name'].toString().codeUnits);
-                      final updateDate = DateFormat('yy.MM.dd')
-                          .format(DateTime.parse(listData['updated_at']));
-                      List<String> keywords = [];
-                      keywords.add(utf8
-                          .decode(listData['keyword_1'].toString().codeUnits));
-                      keywords.add(utf8
-                          .decode(listData['keyword_2'].toString().codeUnits));
-                      print(keywords);
+                child: DynamicHeightGridView(
+                  itemCount: usersBookmarkLists.length,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8.0.w,
+                  mainAxisSpacing: 16.0.h,
+                  builder: (ctx, index) {
+                    return GestureDetector(
+                      onTap: () async {
+                        Map<String, dynamic> listData =
+                            await ApiService.getListDetail(
+                                usersBookmarkLists[index].id);
+                        final title =
+                            utf8.decode(listData['title'].toString().codeUnits);
+                        final description = utf8.decode(
+                            listData['description'].toString().codeUnits);
+                        final userName = utf8
+                            .decode(listData['user_name'].toString().codeUnits);
+                        final updateDate = DateFormat('yy.MM.dd')
+                            .format(DateTime.parse(listData['updated_at']));
+                        List<String> keywords = [];
+                        keywords.add(utf8.decode(
+                            listData['keyword_1'].toString().codeUnits));
+                        keywords.add(utf8.decode(
+                            listData['keyword_2'].toString().codeUnits));
+                        print(keywords);
 
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ListDetailPage(
-                                  title: title,
-                                  description: description,
-                                  userName: userName,
-                                  updateDate: updateDate,
-                                  keywords: keywords,
-                                )),
-                      );
-                    },
-                    child: Stack(
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 218.0.h,
-                              width: double.infinity,
-                              decoration: ShapeDecoration(
-                                color: Color(0xFFF1F3F5),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8)),
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ListDetailPage(
+                                    title: title,
+                                    description: description,
+                                    userName: userName,
+                                    updateDate: updateDate,
+                                    keywords: keywords,
+                                  )),
+                        );
+                      },
+                      child: Stack(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 218.0.h,
+                                width: double.infinity,
+                                decoration: ShapeDecoration(
+                                  color: Color(0xFFF1F3F5),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 12.0.h,
-                            ),
-                            Text(
-                              '${utf8.decode(usersBookmarkLists[index].title.runes.toList())}',
-                              style: TextStyle(
-                                color: Color(0xFF343A40),
-                                fontSize: 14.sp,
-                                fontFamily: 'Pretendard',
-                                fontWeight: FontWeight.w600,
-                                height: 1.42.h,
+                              SizedBox(
+                                height: 12.0.h,
                               ),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                            ),
-                            SizedBox(
-                              height: 4.0.h,
-                            ),
-                            Text(
-                              '${DateFormat('yy.MM.dd').format(DateTime.parse(usersBookmarkLists[index].updatedAt))}',
-                              style: TextStyle(
-                                color: Color(0xFF868E96),
-                                fontSize: 12.sp,
-                                fontFamily: 'Pretendard',
-                                fontWeight: FontWeight.w500,
-                                height: 1.5.h,
+                              Text(
+                                '${utf8.decode(usersBookmarkLists[index].title.runes.toList())}',
+                                style: TextStyle(
+                                  color: Color(0xFF343A40),
+                                  fontSize: 14.sp,
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w600,
+                                  height: 1.42.h,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
                               ),
-                            )
-                          ],
-                        ),
-                        CustomBookMarkButton(
-                          index: index,
-                          listId: usersBookmarkLists[index].id,
-                          isBookMarked: usersBookmarkLists[index].isBookmarked,
-                          tag: 'bookmarkList',
-                        ),
-                      ],
-                    ),
-                  );
-                },
+                              SizedBox(
+                                height: 4.0.h,
+                              ),
+                              Text(
+                                '${DateFormat('yy.MM.dd').format(DateTime.parse(usersBookmarkLists[index].updatedAt))}',
+                                style: TextStyle(
+                                  color: Color(0xFF868E96),
+                                  fontSize: 12.sp,
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w500,
+                                  height: 1.5.h,
+                                ),
+                              )
+                            ],
+                          ),
+                          CustomBookMarkButton(
+                            index: index,
+                            listId: usersBookmarkLists[index].id,
+                            isBookMarked:
+                                usersBookmarkLists[index].isBookmarked,
+                            tag: 'bookmarkList',
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               );
       },
     );
--- lib/widget/list/user_my_list.dart
+++ lib/widget/list/user_my_list.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
@@ -49,14 +50,6 @@ class UserMyList extends StatelessWidget {
                               backgroundColor: Color(0xFFE9ECEF),
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(8)),
-                              // textStyle: TextStyle(
-                              //   color: Color(0xff212529),
-                              //   fontSize: 15.sp,
-                              //   fontFamily: 'Pretendard',
-                              //   fontWeight: FontWeight.w700,
-                              //   height: 1,
-                              // ),
-                              // minimumSize: Size(165.0.w, 52.0.h),
                             ),
                             onPressed: () {
                               Navigator.push(
@@ -82,105 +75,105 @@ class UserMyList extends StatelessWidget {
                   ),
                 ),
               )
-            : GridView.builder(
+            : Padding(
                 padding: EdgeInsets.symmetric(
                   horizontal: 16.0.w,
                   vertical: 20.0.h,
                 ),
-                itemCount: usersMyLists.length, //item 개수
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    childAspectRatio: 0.643,
-                    crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-                    crossAxisSpacing: 8.0.w,
-                    mainAxisSpacing: 16.0.h),
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () async {
-                      Map<String, dynamic> listData =
-                          await ApiService.getListDetail(
-                              usersMyLists[index].id);
-                      final title =
-                          utf8.decode(listData['title'].toString().codeUnits);
-                      final description = utf8
-                          .decode(listData['description'].toString().codeUnits);
-                      final userName = utf8
-                          .decode(listData['user_name'].toString().codeUnits);
-                      final updateDate = DateFormat('yy.MM.dd')
-                          .format(DateTime.parse(listData['updated_at']));
-                      List<String> keywords = [];
-                      keywords.add(utf8
-                          .decode(listData['keyword_1'].toString().codeUnits));
-                      keywords.add(utf8
-                          .decode(listData['keyword_2'].toString().codeUnits));
-                      print(keywords);
+                child: DynamicHeightGridView(
+                  itemCount: usersMyLists.length,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8.0.w,
+                  mainAxisSpacing: 16.0.h,
+                  builder: (ctx, index) {
+                    return GestureDetector(
+                      onTap: () async {
+                        Map<String, dynamic> listData =
+                            await ApiService.getListDetail(
+                                usersMyLists[index].id);
+                        final title =
+                            utf8.decode(listData['title'].toString().codeUnits);
+                        final description = utf8.decode(
+                            listData['description'].toString().codeUnits);
+                        final userName = utf8
+                            .decode(listData['user_name'].toString().codeUnits);
+                        final updateDate = DateFormat('yy.MM.dd')
+                            .format(DateTime.parse(listData['updated_at']));
+                        List<String> keywords = [];
+                        keywords.add(utf8.decode(
+                            listData['keyword_1'].toString().codeUnits));
+                        keywords.add(utf8.decode(
+                            listData['keyword_2'].toString().codeUnits));
+                        print(keywords);
 
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ListDetailPage(
-                                  title: title,
-                                  description: description,
-                                  userName: userName,
-                                  updateDate: updateDate,
-                                  keywords: keywords,
-                                )),
-                      );
-                    },
-                    child: Stack(
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 218.0.h,
-                              width: double.infinity,
-                              decoration: ShapeDecoration(
-                                color: Color(0xFFF1F3F5),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8)),
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ListDetailPage(
+                                    title: title,
+                                    description: description,
+                                    userName: userName,
+                                    updateDate: updateDate,
+                                    keywords: keywords,
+                                  )),
+                        );
+                      },
+                      child: Stack(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 218.0.h,
+                                width: double.infinity,
+                                decoration: ShapeDecoration(
+                                  color: Color(0xFFF1F3F5),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 12.0.h,
-                            ),
-                            Text(
-                              '${utf8.decode(usersMyLists[index].title.runes.toList())}',
-                              style: TextStyle(
-                                color: Color(0xFF343A40),
-                                fontSize: 14.sp,
-                                fontFamily: 'Pretendard',
-                                fontWeight: FontWeight.w600,
-                                height: 1.42.h,
+                              SizedBox(
+                                height: 12.0.h,
                               ),
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                            ),
-                            SizedBox(
-                              height: 4.0.h,
-                            ),
-                            Text(
-                              '${DateFormat('yy.MM.dd').format(DateTime.parse(usersMyLists[index].updatedAt))}',
-                              style: TextStyle(
-                                color: Color(0xFF868E96),
-                                fontSize: 12.sp,
-                                fontFamily: 'Pretendard',
-                                fontWeight: FontWeight.w500,
-                                height: 1.5.h,
+                              Text(
+                                '${utf8.decode(usersMyLists[index].title.runes.toList())}',
+                                style: TextStyle(
+                                  color: Color(0xFF343A40),
+                                  fontSize: 14.sp,
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w600,
+                                  height: 1.42.h,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
                               ),
-                            )
-                          ],
-                        ),
-                        CustomBookMarkButton(
-                          index: index,
-                          listId: usersMyLists[index].id,
-                          isBookMarked: usersMyLists[index].isBookmarked,
-                          tag: 'myList',
-                        ),
-                      ],
-                    ),
-                  );
-                },
+                              SizedBox(
+                                height: 4.0.h,
+                              ),
+                              Text(
+                                '${DateFormat('yy.MM.dd').format(DateTime.parse(usersMyLists[index].updatedAt))}',
+                                style: TextStyle(
+                                  color: Color(0xFF868E96),
+                                  fontSize: 12.sp,
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w500,
+                                  height: 1.5.h,
+                                ),
+                              )
+                            ],
+                          ),
+                          CustomBookMarkButton(
+                            index: index,
+                            listId: usersMyLists[index].id,
+                            isBookMarked: usersMyLists[index].isBookmarked,
+                            tag: 'myList',
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               );
       },
     );
