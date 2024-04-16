dea448b59140bce5e4c278cec1d2853619e3f419
irismake <iris3455@gmail.com>
2024-04-16 21:44:36 +0900

    refactor : Structure user group lists

--- lib/page/book_mark/bookmark_page.dart
+++ lib/page/book_mark/bookmark_page.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,24 +14,6 @@ class BookMarkPage extends StatefulWidget {
 }
 
 class _BookMarkPageState extends State<BookMarkPage> {
-  final _pages = [
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-    '1',
-  ];
-  final _navigatorKeyList = List.generate(5, (index) => ());
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,18 +37,18 @@ class _BookMarkPageState extends State<BookMarkPage> {
               height: 24.0.h,
             ),
             Expanded(
-                child: GridView.count(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8.0.w, //좌우 간격
-              mainAxisSpacing: 16.0.h, //상하 간격
-              childAspectRatio: (175.0.w / 266.0.h),
-              children: _pages.map(
-                (page) {
-                  int index = _pages.indexOf(page);
-                  return BookMarkListWidget(index: index);
+              child: DynamicHeightGridView(
+                itemCount: 10,
+                crossAxisCount: 2,
+                crossAxisSpacing: 8.0.w,
+                mainAxisSpacing: 16.0.h,
+                builder: (ctx, index) {
+                  return GestureDetector(
+                      onTap: () async {},
+                      child: BookMarkListWidget(index: index));
                 },
-              ).toList(),
-            )),
+              ),
+            ),
           ],
         ),
       ),
